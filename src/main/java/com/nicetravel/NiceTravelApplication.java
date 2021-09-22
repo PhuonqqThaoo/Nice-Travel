018e67774b2e220cfc75ab53be6ba96ff589d6e7
Pham Cong Tuan <tuanpc.1902@gmail.com>
2021-09-22 21:04:53 +0700

    first commit

---
+++ src/main/java/com/nicetravel/NiceTravelApplication.java
@@ -0,0 +1,13 @@
+package com.nicetravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicetravel/NiceTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicetravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
