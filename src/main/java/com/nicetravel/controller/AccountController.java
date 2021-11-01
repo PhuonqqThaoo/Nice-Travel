33d146db1c55793707b9b1a58d92cd066d92ab6b
TuanPC <tuanpc.1902@gmail.com>
2021-11-01 18:35:18 +0700

    update form login and register

--- src/main/java/com/nicetravel/controller/AccountController.java
+++ src/main/java/com/nicetravel/controller/AccountController.java
@@ -24,27 +24,28 @@ import java.util.List;
 public class AccountController {
     private final AccountService accountService;
 
-    private final UserServices userServices;
-
     private final PasswordEncoder passwordEncoder;
 
     private final JavaMailSender mailSender;
 
     @Autowired
-    public AccountController(AccountService accountService, UserServices userServices, PasswordEncoder passwordEncoder, JavaMailSender mailSender) {
+    public AccountController(AccountService accountService, PasswordEncoder passwordEncoder, JavaMailSender mailSender) {
         this.accountService = accountService;
-        this.userServices = userServices;
         this.passwordEncoder = passwordEncoder;
         this.mailSender = mailSender;
     }
 
 //    getAll
-    @GetMapping
-    public String getAllAccount(Model model){
-        List<Account> accounts = accountService.getAllAccount();
-        model.addAttribute("accounts", accounts);
-        return "/account/index";
-    }
+//    @GetMapping
+//    public String getAllAccount(Model model){
+//        List<Account> accounts = accountService.getAllAccount();
+//        model.addAttribute("accounts", accounts);
+//        return "/account/index";
+//    }
+//    @GetMapping
+//    public String getAllAccount(){
+//        return "/nice_travel/form-in-up";
+//    }
 
 //    getById
     @GetMapping("{id}")
@@ -59,7 +60,9 @@ public class AccountController {
     public String getAccount(@PathVariable("username") String username, Model model){
         Account account = accountService.findAccountsByUsername(username);
         model.addAttribute("account", account);
-        return "account/index";
+        List<Account> accounts =accountService.getAllAccount();
+        model.addAttribute("accounts", accounts);
+        return "/customer/InformationCustomer";
     }
 
 
@@ -70,7 +73,7 @@ public class AccountController {
         model.addAttribute("account", account);
         List<Account> accounts =accountService.getAllAccount();
         model.addAttribute("accounts", accounts);
-        return "/account/account-detail/info";
+        return "/customer/InformationCustomer";
     }
 
 
@@ -91,99 +94,6 @@ public class AccountController {
     public String delete(@PathVariable("id") Integer id){
         accountService.deleteAccount(id);
         return "redirect:/account";
-    }
-
-
-    //  REGISTER
-
-    @GetMapping("/register")
-    public String showRegistrationForm(Model model) {
-        model.addAttribute("account", new Account());
-        return "/account/register/signup_form";
-    }
-
-    @PostMapping("/process-register")
-    public String processRegister(Account account, HttpServletRequest request)
-            throws UnsupportedEncodingException, MessagingException {
-        userServices.register(account, getSiteURL(request));
-        return "/account/register/register_success";
-    }
-
-    private String getSiteURL(HttpServletRequest request) {
-        String siteURL = request.getRequestURL().toString();
-        return siteURL.replace(request.getServletPath(), "");
-    }
-
-    @GetMapping("/verify")
-    public String verifyUser(@Param("code") String code) {
-        System.out.println(code);
-        if (userServices.verify(code)) {
-            return "/account/register/verify_success";
-        } else {
-            return "/account/register/verify_fail";
-        }
-    }
-
-
-//    Forgot password
-
-
-    @GetMapping("/forgot_password")
-    public String showForgotPasswordForm() {
-        return "/account/forgot/forgot_password_form";
-    }
-
-    @PostMapping("/forgot_password")
-    public String processForgotPassword(HttpServletRequest request, Model model) {
-        String email = request.getParameter("email");
-        String token = RandomString.make(64);
-
-        try {
-            userServices.updateResetPasswordToken(token, email);
-            String resetPasswordLink = Utility.getSiteURL(request) + "/reset_password?token=" + token;
-            userServices.sendEmail(email, resetPasswordLink);
-            model.addAttribute("message", "We have sent a reset password link to your email. Please check.");
-
-        } catch (UsernameNotFoundException ex) {
-            model.addAttribute("error", ex.getMessage());
-        } catch (UnsupportedEncodingException | MessagingException e) {
-            model.addAttribute("error", "Error while sending email");
-        }
-
-        return "/account/forgot/forgot_password_form";
-    }
-
-    @GetMapping("/reset_password")
-    public String showResetPasswordForm(@Param(value = "token") String token, Model model) {
-        Account account = accountService.findByVerificationCode(token);
-        model.addAttribute("token", token);
-
-        if (account == null) {
-            model.addAttribute("message", "Invalid Token");
-            return "message";
-        }
-
-        return "/account/forgot/reset_password_form";
-    }
-
-    @PostMapping("/reset_password")
-    public String processResetPassword(HttpServletRequest request, Model model) {
-        String token = request.getParameter("token");
-        String password = request.getParameter("password");
-
-        Account account = accountService.findByVerificationCode(token);
-        model.addAttribute("title", "Reset your password");
-
-        if (account == null) {
-            model.addAttribute("message", "Invalid Token");
-            return "message";
-        } else {
-            userServices.updatePassword(account, password);
-
-            model.addAttribute("message", "You have successfully changed your password.");
-        }
-
-        return "message";
     }
 
 }
--- src/main/java/com/nicetravel/controller/SecurityController.java
+++ src/main/java/com/nicetravel/controller/SecurityController.java
@@ -1,9 +1,13 @@
 package com.nicetravel.controller;
 
 import com.nicetravel.custom.UserServices;
+import com.nicetravel.custom.Utility;
 import com.nicetravel.entity.Account;
+import com.nicetravel.service.AccountService;
+import net.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +21,22 @@ import java.io.UnsupportedEncodingException;
 @Controller
 public class SecurityController {
 
+    private final UserServices userServices;
+
+    private final AccountService accountService;
+
+    @Autowired
+    public SecurityController(UserServices userServices, AccountService accountService) {
+        this.userServices = userServices;
+        this.accountService = accountService;
+    }
 //    LOGIN
 
-    @RequestMapping("/login")
+    @GetMapping("/login")
     public String loginForm(Model model) {
 //        model.addAttribute("message","Please login");
-        return "/account/login/login";
+        model.addAttribute("account", new Account());
+        return "/nice_travel/form-in-up";
     }
 
     @RequestMapping("/login/success")
@@ -49,5 +63,98 @@ public class SecurityController {
     public String logoffSuccess(Model model) {
         model.addAttribute("message", "Logout success");
         return "forward:/login";
+    }
+
+// REGISTER
+@GetMapping("/register")
+public String showRegistrationForm(Model model) {
+    model.addAttribute("account", new Account());
+    return "/nice_travel/form-in-up";
+}
+
+    @PostMapping("/process-register")
+    public String processRegister(Account account, HttpServletRequest request)
+            throws UnsupportedEncodingException, MessagingException {
+        userServices.register(account, getSiteURL(request));
+        return "/account/register/register_success";
+    }
+
+    private String getSiteURL(HttpServletRequest request) {
+        String siteURL = request.getRequestURL().toString();
+        return siteURL.replace(request.getServletPath(), "");
+    }
+
+    @GetMapping("/verify")
+    public String verifyUser(@Param("code") String code) {
+        System.out.println(code);
+        if (userServices.verify(code)) {
+            return "/account/register/verify_success";
+        } else {
+            return "/account/register/verify_fail";
+        }
+    }
+
+
+//    FORGOT
+
+//    Forgot password
+
+
+    @GetMapping("/forgot_password")
+    public String showForgotPasswordForm() {
+        return "/account/forgot/forgot_password_form";
+    }
+
+    @PostMapping("/forgot_password")
+    public String processForgotPassword(HttpServletRequest request, Model model) {
+        String email = request.getParameter("email");
+        String token = RandomString.make(64);
+
+        try {
+            userServices.updateResetPasswordToken(token, email);
+            String resetPasswordLink = Utility.getSiteURL(request) + "/reset_password?token=" + token;
+            userServices.sendEmail(email, resetPasswordLink);
+            model.addAttribute("message", "We have sent a reset password link to your email. Please check.");
+
+        } catch (UsernameNotFoundException ex) {
+            model.addAttribute("error", ex.getMessage());
+        } catch (UnsupportedEncodingException | MessagingException e) {
+            model.addAttribute("error", "Error while sending email");
+        }
+
+        return "/account/forgot/forgot_password_form";
+    }
+
+    @GetMapping("/reset_password")
+    public String showResetPasswordForm(@Param(value = "token") String token, Model model) {
+        Account account = accountService.findByVerificationCode(token);
+        model.addAttribute("token", token);
+
+        if (account == null) {
+            model.addAttribute("message", "Invalid Token");
+            return "message";
+        }
+
+        return "/account/forgot/reset_password_form";
+    }
+
+    @PostMapping("/reset_password")
+    public String processResetPassword(HttpServletRequest request, Model model) {
+        String token = request.getParameter("token");
+        String password = request.getParameter("password");
+
+        Account account = accountService.findByVerificationCode(token);
+        model.addAttribute("title", "Reset your password");
+
+        if (account == null) {
+            model.addAttribute("message", "Invalid Token");
+            return "message";
+        } else {
+            userServices.updatePassword(account, password);
+
+            model.addAttribute("message", "You have successfully changed your password.");
+        }
+
+        return "message";
     }
 }
