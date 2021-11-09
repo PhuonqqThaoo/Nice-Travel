d065798fe2c1f29c16ec510ac710df589cc131d0
Phuoc Dan <detulaptrinh@gmail.com>
2021-11-09 16:35:36 +0700

    dan update fe booking

--- src/main/resources/static/assets/js/booking-app.js
+++ src/main/resources/static/assets/js/booking-app.js
@@ -20,22 +20,77 @@ app.controller("booking-ctrl", function($scope, $http) {
 	}
 	$scope.booking.loadFromLocalStorage();
 
-	$scope.plus = function() {
+	// btn tăng giảm số lượng
+	$scope.adultPlus = function() {
 		let value = parseInt($('#adult').text());
 		if (value < 10) {
 			value++;
 			$('#adult').text(value)
 		}
 	}
-
-	$scope.minus = function() {
+	$scope.adultMinus = function() {
 		let value = parseInt($('#adult').text());
-		if (value <= 0) {
-			value = 0;
-			$('#adult').text(value)
+		if (value <= 1) {
+			value = 1;
+			$('#adult').text(value);
+			alert("Số lượng tối thiểu là 1");
 		} else {
 			value--;
 			$('#adult').text(value)
+		}
+	}
+
+	$scope.childrenPlus = function() {
+		let value = parseInt($('#children').text());
+		if (value < 10) {
+			value++;
+			$('#children').text(value)
+		}
+	}
+	$scope.childrenMinus = function() {
+		let value = parseInt($('#children').text());
+		if (value <= 0) {
+			value = 0;
+			$('#children').text(value)
+		} else {
+			value--;
+			$('#children').text(value)
+		}
+	}
+
+	$scope.smallchildrenPlus = function() {
+		let value = parseInt($('#smallchildren').text());
+		if (value < 10) {
+			value++;
+			$('#smallchildren').text(value)
+		}
+	}
+	$scope.smallchildrenMinus = function() {
+		let value = parseInt($('#smallchildren').text());
+		if (value <= 0) {
+			value = 0;
+			$('#smallchildren').text(value)
+		} else {
+			value--;
+			$('#smallchildren').text(value)
+		}
+	}
+
+	$scope.babyPlus = function() {
+		let value = parseInt($('#baby').text());
+		if (value < 10) {
+			value++;
+			$('#baby').text(value)
+		}
+	}
+	$scope.babyMinus = function() {
+		let value = parseInt($('#baby').text());
+		if (value <= 0) {
+			value = 0;
+			$('#baby').text(value)
+		} else {
+			value--;
+			$('#baby').text(value)
 		}
 	}
 
