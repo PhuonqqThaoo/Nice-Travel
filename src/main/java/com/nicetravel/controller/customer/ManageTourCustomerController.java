80c9b2799d76155d263d7f9ad6103887a425c601
VanThang <vvt.vovanthang@gmail.com>
2021-11-30 14:57:27 +0700

    thang update huy tour

--- src/main/java/com/nicetravel/controller/customer/ManageTourCustomerController.java
+++ src/main/java/com/nicetravel/controller/customer/ManageTourCustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.nicetravel.entity.Account;
@@ -22,16 +23,24 @@ import com.nicetravel.service.BookingService;
 public class ManageTourCustomerController {
 	@Autowired 
 	BookingService bookingService;
+	@Autowired 
+	BookingDetailService bookingDetailService;
 	@Autowired
 	AccountService accountService;
 	
 	@GetMapping("/tour-da-dat")
 	public String getManageTour(Model model, HttpServletRequest request) {
 		String username = request.getRemoteUser();
-		Account accountId = accountService.findAccountsByUsername(username);
-		
 		List<Booking> items = bookingService.getAllBookingByAcId(username);
 		model.addAttribute("items", items);
 		return "customer/TourDaDat";
+	}
+	@RequestMapping("/isDelete/{id}")
+	public String isDelete(@PathVariable("id") Integer id) {
+		Booking booking = bookingService.findById(id);
+		booking.setIsDeleted(true);
+		bookingService.updateBooking(booking);
+		
+		return "redirect:/customer/tour-da-dat";
 	}
 }
